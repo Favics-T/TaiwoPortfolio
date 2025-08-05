effa02f377ccb5c49741d8be776706d6f8510024
unknown <taiwokolapo533@gmail.com>
2025-08-05 22:15:04 +0100

    done with the nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import svg from './assets/svg.svg'
+import Nav from './component/Nav'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className=' h-screen bg-[url("/assets/grid.svg")] text-white flex  '>
+     <Nav />
     </div>
   )
 }
---
+++ src/component/Nav.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Nav = () => {
+    const list =[
+        {name:"About", path:""},
+         {name:"Projects", path:""},
+          {name:"Experience", path:""},
+          {name:"Home"}
+    
+]
+  return (
+    <div className=' items-center justify-center w-full flex text-white border'>
+      <div className='shadow-xl items-center justify-center rounded-lg bg-[#0c011a] p-4 flex gap-10 fixed top-2 text-white'>
+        {
+            list.map(({name,path})=>(
+                <li className='list-none cursor-pointer hover:text-pink-700'>{name}</li>
+            ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Nav
